7be5ed4b736921d11894bd7770c2c78efc3a96b0
kowbix <kacperkowbel63@gmail.com>
2024-03-03 23:23:07 +0100

    Init project

---
+++ otomoto-clon/src/main/java/org/example/otomotoclon/OtomotoClonApplication.java
@@ -0,0 +1,13 @@
+package org.example.otomotoclon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtomotoClonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtomotoClonApplication.class, args);
+    }
+
+}
---
+++ otomoto-clon/src/test/java/org/example/otomotoclon/OtomotoClonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.otomotoclon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtomotoClonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
