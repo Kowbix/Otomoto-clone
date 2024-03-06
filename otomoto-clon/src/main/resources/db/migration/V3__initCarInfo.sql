df847505a654afd4dccba4023e20fec8b323841c
kowbix <kacperkowbel63@gmail.com>
2024-03-06 21:17:21 +0100

    Init Car table & change sql which init car details

--- otomoto-clon/src/main/resources/db/migration/V3__initCarInfo.sql
+++ otomoto-clon/src/main/resources/db/migration/V3__initCarInfo.sql
@@ -3,15 +3,7 @@ create table if not exists bodywork_types (
     name VARCHAR(255) NOT NULL
 );
 
-create table if not exists bodyworks (
-    id SERIAL PRIMARY KEY,
-    doors_count int,
-    seats_count int,
-    bodywork_type_id BIGINT,
-    foreign key (bodywork_type_id) references bodywork_types(id)
-);
-
-create table if not exists fueals (
+create table if not exists fuel (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
@@ -21,20 +13,7 @@ create table if not exists damage_conditions (
     name VARCHAR(255) NOT NULL
 );
 
-create table if not exists images (
-    id SERIAL PRIMARY KEY,
-    url VARCHAR(255) NOT NULL
-);
-
 create table if not exists transmissions (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
-);
-
-create table if not exists engines (
-    id SERIAL PRIMARY KEY,
-    capacity int,
-    horsepower int,
-    transmission_id BIGINT,
-    foreign key(transmission_id) references transmissions(id)
 );
---
+++ otomoto-clon/src/main/resources/db/migration/V4__initCar.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS engines (
+   id SERIAL PRIMARY KEY,
+   capacity INT NOT NULL,
+   horsepower INT NOT NULL
+);
+
+
+CREATE TABLE if not exists cars (
+    id SERIAL PRIMARY KEY,
+    brand_id BIGINT NOT NULL,
+    model_id BIGINT NOT NULL,
+    generation_id BIGINT,
+    year_production INTEGER,
+    bodywork_type_id BIGINT NOT NULL,
+    fuel_id BIGINT NOT NULL,
+    millage INTEGER,
+    damage_condition_id BIGINT NOT NULL,
+    vin VARCHAR(255),
+    engine_id BIGINT,
+    transmission_id BIGINT NOT NULL,
+    door_count INTEGER,
+    seat_count INTEGER,
+    FOREIGN KEY (brand_id) REFERENCES brands(id),
+    FOREIGN KEY (model_id) REFERENCES models(id),
+    FOREIGN KEY (generation_id) REFERENCES generations(id),
+    FOREIGN KEY (bodywork_type_id) REFERENCES bodywork_types(id),
+    FOREIGN KEY (fuel_id) REFERENCES fuel(id),
+    FOREIGN KEY (damage_condition_id) REFERENCES damage_conditions(id),
+    FOREIGN KEY (engine_id) REFERENCES engines(id),
+    FOREIGN KEY (transmission_id) REFERENCES transmissions(id)
+);
+
+CREATE TABLE if not exists image (
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(255) NOT NULL,
+    car_id INT REFERENCES cars(id)
+);
